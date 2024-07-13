9f4909b455aeb150c26b877c438d710cde6ff0ac
ahmedmehmoodcb <72783053+ahmedmehmoodcb@users.noreply.github.com>
2024-07-14 01:57:52 +0500

    Day_1_Variablesand_Data_Types
    Day_1_Variablesand_Data_Types

---
+++ Day_1_Variablesand_Data_Types/main.js
@@ -0,0 +1,65 @@
+// First day task
+
+// Task 1
+var number = 1;
+console.log(number);
+
+// Task 2
+let string ="string";
+console.log(string);
+
+// Task 3 Boolean
+const Boolean = 5 > 6
+console.log(Boolean);
+
+// Data type (number, string, boolean, object, array)
+let dataTypeNumber = 10
+console.log(typeof dataTypeNumber);
+
+// string
+let dataTypeString = "10"
+console.log(typeof dataTypeString);
+
+// boolean
+let dataTypeBoolean = true;
+console.log(typeof dataTypeBoolean);
+
+// object
+let dataTypeObject = {
+    firstName: "ahmed",
+    seondName:"mehmood",
+} ;
+console.log(typeof dataTypeObject);
+
+// Array
+let dataTypeArray = ["1", "2", "3", "4", "5"];
+console.log(typeof dataTypeArray);
+
+// Activity 4: Reassigning Variables
+let reassigningVariables = "first value";
+reassigningVariables = "second Value"
+console.log (reassigningVariables);
+
+// Activity 5: Understanding
+const reassigningVariablesConst = "first value";
+// reassigningVariablesConst = "second Value"
+console.log (reassigningVariablesConst);
+
+
+// Feature Request:
+let num = 25;
+console.log(num); // Outputs: Value: 42 Type: number
+
+let str = "Str";
+console.log(str);
+
+let isTrue = true;
+console.log(isTrue)
+
+let obj = {
+    firstName: "ahmed",
+    lastName: "mehmood",
+    age:30,
+}
+console.log(obj);
+
